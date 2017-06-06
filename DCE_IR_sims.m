f21f0da897c5d144daf139db73205ed66f2857c2
jschoormans <jasper.schoormans@gmail.com>
2017-06-06 14:59:58 +0200

    first commit
    blabla

---
+++ DCE_IR_sims.m
@@ -0,0 +1,44 @@
+addpath(genpath('/home/jschoormans/lood_storage/divi/Projects/cosart/Matlab_Collection/nway320 exchange'))
+res=64
+nt=100; 
+nTI=40; 
+
+A=phantom(res);
+
+A=repmat(A,[1 1 nt nTI]);
+
+B=1-2*exp(-linspace(0,2,nTI));
+B=permute(B,[1 3 4 2]); % set b dimenion too 4(nti)
+B=repmat(B,[res res nt 1]);
+
+% C: time dimensinos
+C=rand(1,1,nt,1);
+C=repmat(C,[res res 1 nTI]);
+
+% make image;
+
+D=A.*B.*C;
+
+size(D)
+size(C)
+size(B)
+size(A)
+
+%% TUCKER DEMCOPOSITION
+
+E=reshape(D,[res.^2, nt nTI]); % group dims
+
+E=E+randn(size(E)).*0.001;
+a=10
+b=10
+c=10
+
+[F1,F2,F3,F4]=tucker(E,[a b c]);
+
+F3
+%%
+
+
+disp('fraction of original information needed:')
+(res^2*nt*nTI)./(res^2*a+nt*b+nTI*c+a*b*c)
+disp('pct of orignal signal explained:' ); F3
---
+++ diffusion_phantom.m
@@ -0,0 +1,24 @@
+addpath(genpath('/home/jschoormans/lood_storage/divi/Projects/cosart/Matlab_Collection/nway320 exchange'))
+%% @2Dt (eg diffusion /DCE whatever)000000000000000000
+
+res=64;
+A=phantom(res);
+B=exp(-linspace(0,2,res));
+C=repmat(B,[res 1 res]); C=permute(C,[1 3 2]);
+R=rand(res,res)>0.5; R=repmat(R,[1 1 res]);
+D=repmat(A,[1 1 res]);
+
+E=C+C.*D.*R;
+
+
+%%
+ %tensor ranks
+a=10
+b=a;
+c=2;
+
+[F1,F2,F3,F4]=tucker(E,[a,b,c]);
+
+disp('fraction of original information needed:')
+res^3./(res*a+res*b+res*c+a*b*c)
+disp('pct of orignal signal explained:' ); F3
