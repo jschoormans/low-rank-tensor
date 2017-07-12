057de8bf7acdf9e132d69f8f2b1fcea891035431
jschoormans <jasper.schoormans@gmail.com>
2017-07-12 15:30:38 +0200

    precon test: add jacobi preconditioner to general code

--- functions/precon_conj_grad_G.m
+++ functions/precon_conj_grad_G.m
@@ -2,32 +2,36 @@ function Gk=precon_conj_grad_G(G,C,A,Y,alpha,Psi,d,Phi,F)
 tic; 
 fprintf('---conjugate gradient algorithm: solving for G_k ---- \n')
 
-
-
 tol=1e-10;
-maxiter=200;
 maxiter=50; %temp
 
 L=Lambda(F,C,Phi,abs(d)>0);
 a2PP=(alpha/2)*Psi'*Psi;
 
+%input data
 b=(L'*d + (alpha/2)*Psi'*(A+Y./alpha));
 b=b(:);
-% try to reshape operator so we have proper matrix, vector calculations
 
+%build preconditioner
+CPPC=(C*Phi*Phi'*C');
+w=diag(CPPC)+alpha/2; 
+w=permute(w,[2 1]);
+w=1./w;
+mfun=@(x) Res(bsxfun(@times, ResA(x),w)); %test
+
+% try to reshape operator so we have proper matrix, vector calculations
 X= @(G) (L'*(L*G)) +a2PP*G;
 Res= @(x) reshape(x,[numel(G),1]);
 ResA= @(x) reshape(x,size(G));
 Aop = @(G) Res(X(ResA(G))); 
 
-[x,flag,relres,iter,resvec]=bicgstab(Aop,b,tol,maxiter,[],[],G(:)); %add initial guess
 
-% [x2,flag,relres,iter,resvec1]=cgs(Aop,b,tol,maxiter); %seems fastest
-% [x2,flag,relres,iter,resvec2]=bicgstab(Aop,b,tol,maxiter);
-% [x2,flag,relres,iter,resvec3]=pcg(Aop,b,tol,maxiter);
+
+[x,flag,relres,iter,resvec]=bicgstab(Aop,b,tol,maxiter,mfun,[],G(:)); %add initial guess
+
 
 Gk=ResA(x);
-figure(19); plot(log(resvec)); 
+figure(19); plot(log10(resvec)); 
 
 t=toc; 
 fprintf('Time taken: %d seconds',t)
