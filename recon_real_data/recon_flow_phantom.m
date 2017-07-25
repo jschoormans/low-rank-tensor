df9645733ac3e6d66789e0697eb779dad5e15426
hisgoodbuddy <qw.kerry.zhang@gmail.com>
2017-07-25 13:03:20 +0200

    changes related to DTI-T2prep implementation

--- recon_real_data/recon_flow_phantom.m
+++ recon_real_data/recon_flow_phantom.m
@@ -2,12 +2,7 @@ clear all;
 close all; 
 clc
 % cd('/home/jschoormans/lood_storage/divi/Temp/Lukas/reconstruction/test-data/spiralshape')
-
-if ispc()
 cd('L:\basic\divi\Temp\Lukas\reconstruction\test-data\spiralshape')
-else
-    cd('/home/jschoormans/lood_storage/divi/Temp/Lukas/reconstruction/test-data/spiralshape')
-end
 file='cs_12012017_1913584_9_2_wipk64r6mode1np50t3flip1senseV4.raw'
 % file='cs_12012017_1905246_8_2_wipk64r6mode1np50t3flip0senseV4.raw'
 %% copied from lukas' code'
@@ -66,82 +61,81 @@ clear Q J
 
 %% estimate subspaces
 addpath(genpath('/home/jschoormans/lood_storage/divi/Projects/cosart/CS_simulations/tensor/low-rank-tensor'))
-sparsity_transform='TVcomplex'
 
-res1=size(du,1);
-res2=size(du,2);
-L3=2;
-L4=2;
+L3=4;
+L4=3;
 
-ctr=1
-ctrcoords1=32-ctr:32+ctr %???
-ctrcoords2=32-ctr:32+ctr
+ctr=10
+ctrcoords=33-ctr:33+ctr %???
+
 % >>>>>>>>>>>>>>>>>>>>RECON FROM HERE<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 % 2: estimate subspaces
-nav_parameter_dim1 = squeeze(du(ctrcoords1,ctrcoords2,:,:,1));
+% nav_parameter_dim1 = squeeze(du(ctrcoords,ctrcoords,:,:,1));
+nav_parameter_dim1 = squeeze(du(ctrcoords,ctrcoords,:,:,:));
+nav_parameter_dim1=(sum(nav_parameter_dim1,5)./(sum(nav_parameter_dim1~=0,5)+eps));
+% assert(sum(nav_parameter_dim1(:)==0)==0);
 nav_estimate_1= subspace_estimator_multicoil(nav_parameter_dim1,L3);
 
-nav_parameter_dim2 = squeeze(du(ctrcoords1,ctrcoords2,:,1,:));
-nav_estimate_2= subspace_estimator_multicoil(nav_parameter_dim2,L4);
+nav_parameter_dim2 = squeeze(du(ctrcoords,ctrcoords,:,:,:));
+nav_parameter_dim2=(sum(nav_parameter_dim2,4)./(sum(nav_parameter_dim2~=0,4)));
+nav_parameter_dim2=squeeze(nav_parameter_dim2);
+% % assert(sum(nav_parameter_dim2(:)==0)==0);
+% 
 
+nav_estimate_2= subspace_estimator_multicoil(nav_parameter_dim2,L4);
+figure(2); clf
+plot(real(nav_estimate_1)); hold on
+% plot(imag(nav_estimate_1),'k'); hold off
+figure(3); clf
+plot(real(nav_estimate_2),'r'); hold on
+plot(imag(nav_estimate_2),'k'); hold off
+
+%%
 tensorsize=size(du);
 imagesize=tensorsize; imagesize(3)=1; 
 unfoldedIsize=[size(du,1)*size(du,2),size(du,4)*size(du,5)];                %coil combined
 unfoldedKsize=[size(du,1)*size(du,2)*size(du,3),size(du,4)*size(du,5)];     %coils separate
 
+sens=squeeze(sens)
+res=64
+sparsity_transform='TV'
 % 3: initialize other operators
 if strcmp(sparsity_transform,'wavelet')
     Psi=opWavelet2(res,res,'Daubechies') %wavelet operator (uses SPOT toolbox (+ other dependencies maybe?)
 elseif strcmp(sparsity_transform,'TV')
     % to do: can be made ~15 times faster with a finite difference operator
-%     Psi=opConvolve(res,res,[-1 1],[0 0],'truncated')* opConvolve(res,res,[-1 1]',[0 0],'truncated') %2D TV operator
-elseif strcmp(sparsity_transform,'TVcomplex')
-%     Psi=TV2op()
-    Psi1=opConvolve(res1,res2,[-1 1],[0 0],'truncated')
-    Psi2=opConvolve(res1,res2,[-1 1]',[0 0],'truncated')
-    Psi=[Psi1;Psi2]
+    Psi=opConvolve(res,res,[-1 1],[0 0],'truncated')* opConvolve(res,res,[-1 1]',[0 0],'truncated') %2D TV operator
 end
 
-sens=squeeze(sens);
-sens_normalized=bsxfun(@rdivide,sens,(eps+sqrt(sum(abs(sens).^2,3)))); 
-F=MCFopClass;
-set_MCFop_Params(F,(sens_normalized),[res1,res2],[tensorsize(4),tensorsize(5)]);
+sens_normalized=bsxfun(@rdivide,sens,sqrt(sum(abs(sens+eps).^2,3))); 
+F=MCFop([res,res],(sens_normalized));
 
 %4 zero-filled recon
 P0=F'*du;             
 P1_0=reshape(P0,unfoldedIsize); %1-unfolding of zero filled recon (what is the shape of this matrix?)
 du_1=reshape(du,unfoldedKsize);
 
-figure(4); imshow(abs(P0(:,:,1,1,1)),[]); axis off; title('zero filled recon of one frame')
-figure(5); imshow(angle(P0(:,:,1,1,1)),[]); axis off; title('phase of zero filled recon of one frame')
-figure(6); immontage4D(squeeze(abs(P0)));
-figure(7); immontage4D(squeeze(angle(P0)),[0 2*pi]);
-
-%% 
-% lowres_phase_estimate=exp(-1i*angle(P0(:,:,1,1,1)));
-% Psi=Psi*opDiag(lowres_phase_estimate(:))
-
+figure(4); imshow(cat(2,abs(P0(:,:,1,10,4))./max(abs(P0(:))),angle(P0(:,:,1,15,1))./(2*pi)),[]); axis off; title('zero filled recon of one frame')
 %% ALGO 
 %initialize parameters
 alpha= 2;         %penalty parameter >0
 beta=  2;         %penalty parameter >0
-alpha=0.01/(mean(abs(du(du~=0))));
-beta=alpha; %from paper 
+% alpha=0.01/(mean(abs(du(du~=0))));beta=alpha; %from paper 
 
-lambda=1e-8;        %sparsity parameter
-mu=1e-1;           %sparsity parameter
+lambda=5e-1;        %sparsity parameter
+mu=5e-1 ;           %sparsity parameter
 
-% Lg=L3*L4;             %rank of spatial dimension
-Lg=3;
+Lg=L3*L4;             %rank of spatial dimension
+Lg=5;
 niter=10;
 
 %initialize matrices
-[Phi,G,C,A,B,Y,Z]= init_G0(P1_0,Psi,nav_estimate_1,nav_estimate_2,Lg);                    
+[Phi,G,C,A,B,Y,Z]= init_G0(P1_0,nav_estimate_1,nav_estimate_2,Lg);                    
 
 MSE=[]; 
 for iter=1:niter
-    MSE=visualize_convergence(iter,MSE,G,C,Phi,[],imagesize,20,33);
+    MSE=visualize_convergence(iter,MSE,G,C,Phi,[],imagesize,17,39);
     Ak=soft_thresh_A(G,Y,alpha,lambda,Psi);                     %15
     Bk=soft_thresh_B(C,Z,mu,beta);                              %16
     Gk=precon_conj_grad_G(G,C,Ak,Y,alpha,Psi,du_1,Phi,F);       %17
@@ -153,3 +147,40 @@ for iter=1:niter
     
 % alpha=alpha*1.5; beta=beta*1.5;    %optional: increasing beta & alpha
 end
+%%
+P_recon=G*C*Phi;
+
+P_recon=reshape(P_recon,imagesize);
+
+figure(998); 
+ Q=[]
+for ii=1:size(P_recon,4)
+    J=[];
+    for jj=1:size(P_recon,5);
+        J=[J,angle(P_recon(:,:,1,ii,jj))];
+    end
+    Q=[Q;J];
+end
+figure(100)
+imshow((Q),[])
+clear Q J 
+
+%% 
+
+figure(998); 
+ Q=[]
+for ii=15:24
+    J=[];
+    for jj=1:size(P_recon,5);
+        J=[J,angle(P0(:,:,1,ii,jj))];
+    end
+    Q=[Q;J];
+end
+figure(101)
+imshow((Q),[])
+clear Q J 
+
+
+
+
+
