5af1e41098720b2dc23e23e370986d9ec7cfc1cf
jschoormans <jasper.schoormans@gmail.com>
2017-07-27 21:02:29 +0200

    cleaning up simulation code

---
+++ old code/create_undersampled_measurement.m
@@ -0,0 +1,73 @@
+% making undersampled k-space measurement
+% with fully sampled navigator k-space centers 
+
+% TO DO: -add var density and more us options
+rng('default') 
+rng(3); % random seed to control phantom generation
+res=128;
+
+ADCvals=[1:5].*1e-3;
+T1vals=[20 50 100 200 800].*1e-3;
+
+TE=[10:10:100].*0.001 % in seconds
+bvals=[0:100:900] %units?
+
+I=diffusion_T1_phantom(res,ADCvals,T1vals,TE,bvals,1);
+
+figure(1); Q=[]
+for ii=1:size(I,3)
+    J=[];
+    for jj=1:size(I,4);
+        J=[J,abs(I(:,:,ii,jj))];
+    end
+    Q=[Q;J];
+end
+imshow(abs(Q),[0 1])
+xlabel('echo times')
+ylabel('b-values')
+clear Q J 
+%% to k space 
+d=fftshift(fftshift(fft(fft(ifftshift(ifftshift(I,2),1),[],1),[],2),1),2)./sqrt(res*res);
+figure(2); imshow(abs(d(:,:,1,1)),[])
+
+%% make undersampling mask  
+mask=rand(size(d))>(1-uf); %undersampling
+
+% add center for subspace estimae
+ctr=10;
+ctrcoords=floor(res/2)-ctr: floor(res/2)+ctr; 
+ll=length(ctrcoords);
+mask(ctrcoords,ctrcoords,1,:)=ones(ll,ll,1,size(d,4));
+mask(ctrcoords,ctrcoords,:,1)=ones(ll,ll,1,size(d,4));
+
+% for all measurements
+ctrcoordsall=floor(res/2)-3: floor(res/2)+3; 
+mask(ctrcoordsall,ctrcoordsall,:,:)=ones(7,7,size(d,3),size(d,4));
+
+figure(3); Q=[]
+for ii=1:size(I,3)
+    J=[];
+    for jj=1:size(I,4);
+        J=[J,abs(mask(:,:,ii,jj))];
+    end
+    Q=[Q;J];
+end
+imshow(abs(Q),[0 1]); clear Q J 
+%% make undersampled measurement; 
+du=mask.*d; 
+
+clear d 
+
+%add noise
+if noiselevel>0
+du=du+(randn(size(du)).*mean(du(:)).*noiselevel).*(du~=0);
+end
+
+
+
+
+
+
+
+
+
---
+++ old code/draft_reconstruction_code.m
@@ -0,0 +1,81 @@
+% draft reconstruction code 
+clear all; close all; clc;
+
+% 1: make data (most settings in other .m file for now)
+
+uf=0.01; % undersampling factor (excluding center)
+noiselevel=0;
+sparsity_transform='wavelet'
+% sparsity_transform='TV'
+
+run create_undersampled_measurement.m    
+
+
+% 2: estimate subspaces
+L3=4;               %rank of subspace dimension 3
+L4=6;               %rank of subspace dimension 4
+
+nav_parameter_dim1 = squeeze(du(ctrcoords,ctrcoords,:,1));
+nav_estimate_1= subspace_estimator(nav_parameter_dim1,L3);
+
+nav_parameter_dim2 = squeeze(du(ctrcoords,ctrcoords,1,:));
+nav_estimate_2= subspace_estimator(nav_parameter_dim2,L4);
+
+% 3: initialize other operators
+tensorsize=size(du);
+unfoldedsize=[size(du,1)*size(du,2),size(du,3)*size(du,4)];
+
+F=Fop([res,res]);
+
+if strcmp(sparsity_transform,'wavelet')==1
+Psi=opWavelet2(res,res,'Daubechies') %wavelet operator (uses SPOT toolbox (+ other dependencies maybe?) 
+else
+Psi=opConvolve(res,res,[-1 1],[0 0],'truncated')* opConvolve(res,res,[-1 1]',[0 0],'truncated') %2D TV operator
+end
+
+%4 zero-filled recon
+P0=F'*du;
+P1_0=reshape(P0,unfoldedsize); %1-unfolding of zero filled recon (what is the shape of this matrix?)
+du_1=reshape(du,unfoldedsize);
+
+figure(4); imshow(abs(P0(:,:,2,2)),[])
+%% ALGO 
+%initialize parameters
+alpha= 0.2;         %penalty parameter >0
+beta=  0.2;         %penalty parameter >0
+lambda=5e-2;        %sparsity parameter
+mu=1e-2 ;           %sparsity parameter
+Lg=400;             %rank of spatial dimension
+niter=50;
+
+%initialize matrices
+[Phi,G,C,A,B,Y,Z]= init_G0(P1_0,nav_estimate_1,nav_estimate_2,Lg);                    
+
+MSE=[]; 
+for iter=1:niter
+    MSE=visualize_convergence(iter,MSE,G,C,Phi,I,tensorsize,61,33)
+    
+    Ak=soft_thresh_A(G,Y,alpha,lambda,Psi);             %15
+    Bk=soft_thresh_B(C,Z,mu,beta);                      %16
+    Gk=precon_conj_grad_G(G,C,Ak,Y,alpha,Psi,du_1,Phi,F);    %17 to do...
+    Ck=conj_grad_C_3(Gk,C,Bk,Z,beta,du_1,Phi,F);        %18 to do...
+    Yk=Y+alpha*(Ak-Psi*Gk);
+    Zk=Z+beta.*(Bk-Ck);
+    
+    G=Gk; C=Ck; Y=Yk; Z=Zk; %update iteration
+end
+P_recon=G*C*Phi;
+P_recon=reshape(P_recon,tensorsize);
+%%
+figure(998); 
+ Q=[]
+for ii=1:size(I,3)
+    J=[];
+    for jj=1:size(I,4);
+        J=[J,abs(P_recon(:,:,ii,jj))];
+    end
+    Q=[Q;J];
+end
+imshow(abs(Q),[0 0.1])
+clear Q J 
+%%
