b56b1d9823c699ffcd469f8a96d7a48826e1f5c2
jschoormans <jasper.schoormans@gmail.com>
2017-07-31 15:29:53 +0200

    added option to use different dimension numbers for subspace estimate + flow recon

--- LRT_recon.m
+++ LRT_recon.m
@@ -39,18 +39,18 @@ xlabel('Parameter 1'); ylabel('Parameter 2');
 
 
 % >>>>>>>>>>>>>>>>>>>>RECON FROM HERE<<<<<<<<<<<<<<<<<<<<<<<<<<<
-[Kx1,Ky1,Kx2,Ky2]=findSharedKpoints(mask);
+[Kx1,Ky1,Kx2,Ky2]=findSharedKpoints(mask,params);
 
 % 2: estimate subspaces: generalized for non-square shared k-points
 nav_parameter_dim1=[];
 for iter=1:length(Kx1)
-    nav_parameter_dim1=cat(1,nav_parameter_dim1,(kspace(Kx1(iter),Ky1(iter),:,:,1)));
+    nav_parameter_dim1=cat(1,nav_parameter_dim1,(kspace(Kx1(iter),Ky1(iter),:,:,params.subspacedim1)));
 end
 [nav_estimate_1,eigenvals_1]= subspace_estimator_multicoil(squeeze(nav_parameter_dim1),params.L3);
 
 nav_parameter_dim2=[];
 for iter=1:length(Kx2)
-    nav_parameter_dim2=cat(1,nav_parameter_dim2,(kspace(Kx2(iter),Ky2(iter),:,1,:)));
+    nav_parameter_dim2=cat(1,nav_parameter_dim2,(kspace(Kx2(iter),Ky2(iter),:,params.subspacedim2,:)));
 end
 [nav_estimate_2,eigenvals_2]= subspace_estimator_multicoil(squeeze(nav_parameter_dim2),params.L4);
 
