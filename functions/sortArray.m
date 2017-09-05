2faa5c68de42ac18a1569281500028a3206ed9c3
jschoormans <jasper.schoormans@gmail.com>
2017-09-05 13:42:33 +0200

    improved sortarray - however parallelization should be done before using
    whole data matrixtakes too much memory - either bart cc or ifft in chunks???

--- functions/sortArray.m
+++ functions/sortArray.m
@@ -1,13 +1,19 @@
 function D= sortArray(MR)
 % function to sort MR.Data if normal MR.SortData crashes
-% sorts based on dynamics and channels... (other things would not be sorted
+% sorts based on dynamics, averages and channels... (other things would not be sorted
 % correctly as of now) 
 
-ky=MR.Parameter.Labels.Index.ky;
-kz=MR.Parameter.Labels.Index.kz;
-chan=MR.Parameter.Labels.Index.chan;
-param1=MR.Parameter.Labels.Index.dyn;
+
+ky=MR.Parameter.Labels.Index.ky((MR.Parameter.Labels.Index.typ==1));
+kz=MR.Parameter.Labels.Index.kz(MR.Parameter.Labels.Index.typ==1);
+chan=MR.Parameter.Labels.Index.chan(MR.Parameter.Labels.Index.typ==1);
+param1=MR.Parameter.Labels.Index.dyn(MR.Parameter.Labels.Index.typ==1);
+param2=MR.Parameter.Labels.Index.aver(MR.Parameter.Labels.Index.typ==1);
+
 MRD=MR.Data;
+
+assert(numel(chan)==length(MRD),'subsets of data not yet supported ')
+
 
 nkx=size(MR.Data,1);
 
@@ -31,6 +37,14 @@ minp1=min(p1unique);
 maxp1=max(p1unique);
 np1=length(p1unique);
 
+p2unique=unique(param2+1);
+minp2=min(p2unique);
+maxp2=max(p2unique);
+np2=length(p2unique);
+
+assert(minp2==1) 
+assert(minp1==1)
+
 %input chan number, outputs index of chan in inuqie chan list
 chantransform=zeros(numel(chanunique),1);
 for ii=1:numel(chanunique)
@@ -44,21 +58,29 @@ p1indextransform(p1unique(ii))=ii; %[0,1,2,3,4,]
 end
 
 
-D=zeros(nkx,nky,nkz,nchans,np1);
+%input chan number, outputs index of chan in inuqie chan list
+p2indextransform=zeros(numel(p2unique),1);
+for ii=1:numel(p2unique)
+p2indextransform(p2unique(ii))=ii; %[0,1,2,3,4,]
+end
+
+D=sparse(nkx,nky,nkz,nchans,np1,np2);
 
 kyindex=ky-minky+1;
 kzindex=kz-minkz+1;
 chanindex= chantransform(chan); % should be able to deal with eg chan=2,3,5,6
 np1index= p1indextransform(param1+1); % TO DO: CHANGE: 
+np2index=p2indextransform(param2+1); 
 
 fprintf('sorting MR.Data \n')
 fprintf('number of unique channels %d \n',nchans)
 fprintf('number of unique dynamics %d \n',np1)
+fprintf('number of unique averages %d \n',np2)
 
 tic
 for ii=1:size(MRD,2)
     if mod(ii,1e3)==0;waitbar(ii/size(MRD,2));end
-    D(:,kyindex(ii),kzindex(ii),chanindex(ii),np1index(ii))=MR.Data(:,ii);
+    D(:,kyindex(ii),kzindex(ii),chanindex(ii),np1index(ii),np2index(ii))=MR.Data(:,ii);
 end
 toc
 
