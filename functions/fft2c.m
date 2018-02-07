8298d584f88607b250e8e35b7e225eaf543b5f75
jschoormans <jasper.schoormans@gmail.com>
2018-02-07 16:41:00 +0100

    changing to fft2
    fft operations twice as fast (most expensive ops by far)

--- functions/fft2c.m
+++ functions/fft2c.m
@@ -9,7 +9,8 @@ res = zeros(size(x));
 % for ii=1:size(x,3)
 %         res(:,:,ii) = sqrt(fctr)*fftshift(ifft2(ifftshift(x(:,:,ii))));
 % endx 
-res = (1/sqrt(fctr))*fftshift(fftshift(fft(fft(ifftshift(ifftshift(x,1),2),[],1),[],2),1),2);
+% res = (1/sqrt(fctr))*fftshift(fftshift(fft(fft(ifftshift(ifftshift(x,1),2),[],1),[],2),1),2);
+res = (1/sqrt(fctr))*fftshift(fftshift(fft2(ifftshift(ifftshift(x,1),2)),1),2); %twice as fast (on GPU at least!) 
 
 % res = reshape(res, size_x);
 
